f7594ae5e2df1c1a256748ea7be484b6b5db4d66
Jonathan Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2026-05-01 20:15:50 -0600

    Initial commit

---
+++ ci-cd/Program.cs
@@ -0,0 +1,6 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Content("<h1>Hola Mundo desde Azure !!!!!</h1>", "text/html"));
+
+app.Run();
