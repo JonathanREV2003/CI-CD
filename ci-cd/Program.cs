8246221f4117e2a2a881316d9ac34adc19a67d7d
Jonathan Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2026-05-01 22:57:19 -0600

    Commit de CI-CD

--- ci-cd/Program.cs
+++ ci-cd/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Content("<h1>Hola Mundo desde Azure ¿Ya wey?</h1>", "text/html"));
+app.MapGet("/", () => Results.Content("<h1>Hola Mundo desde Azure Actualizacón continua aprobada</h1>", "text/html"));
 
 app.Run();
